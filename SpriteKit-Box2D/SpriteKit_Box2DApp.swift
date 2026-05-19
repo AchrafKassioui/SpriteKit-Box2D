d6d272b88a8efbe17ea6aec6c7661fa5a6cf205c
Achraf Kassioui <achrafkassioui@gmail.com>
2026-05-19 18:13:43 +0100

    Initial Commit

---
+++ SpriteKit-Box2D/SpriteKit_Box2DApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKit_Box2DApp.swift
+//  SpriteKit-Box2D
+//
+//  Created by Achraf Kassioui on 19/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKit_Box2DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
